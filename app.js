054ccef9312a53ad94f6e973c2e66ec09c0cc45e
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-10 23:52:18 +0600

    //Copy nested reference immutably //use loadash

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //Copy nested reference immutably
-
+//use loadash
 var language = {
     name: 'JavaScript',
     estd: '1995',
     chars: ['a', 'c', 'd'],
 };
 
-var language2 = { ...language, chars: [...language.chars] };
+var language2 = { ...language };
 
 language2.chars.push(5);
 
 console.log(language);
+console.log(language2);
